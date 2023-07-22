de75244dbfda3d4ed6a6afcb33dbf7baf6aeb6da
David Warwick <bgmsd@live.com>
2023-07-22 11:52:48 -0700

    test database migrations

--- ForumAppJWTAzure/Server/GlobalUsings.cs
+++ ForumAppJWTAzure/Server/GlobalUsings.cs
@@ -4,4 +4,4 @@ global using Microsoft.Extensions.Hosting;
 global using System.Collections.Generic;
 global using System.Reflection.Emit;
 global using Microsoft.EntityFrameworkCore;
-global using System.ComponentModel.DataAnnotations; 
+global using System.ComponentModel.DataAnnotations;
